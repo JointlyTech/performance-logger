1ebea1ef283c7f4655ef344a5f57c46b2946bd0b
Michael Di Prisco <cadienvan@gmail.com>
2022-12-20 15:31:08 +0100

    refactor: used hook-fn instead of reimplementing it

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,7 @@
+import { hook } from 'hook-fn';
 import { performance } from 'perf_hooks';
 import { LogPerformanceDefault } from './default';
-import { LogPerformanceOptions, LogPerformanceOptionsLog } from './models';
+import { LogPerformanceOptions } from './models';
 
 export function LogPerformance(_options: Partial<LogPerformanceOptions> = {}) {
   const options: LogPerformanceOptions = {
@@ -14,28 +15,10 @@ export function LogPerformance(_options: Partial<LogPerformanceOptions> = {}) {
     descriptor: PropertyDescriptor
   ) {
     // If name is null, set it to the name of the class and the function
-    const name = options.name ?? getName({ target, propertyKey });
+    options.name = options.name ?? getName({ target, propertyKey });
     const originalMethod = descriptor.value;
     descriptor.value = function (...args: any[]) {
-      if (!hasPassedValidation({ validation: options.validation, args }))
-        return originalMethod.apply(this, args);
-
-      const start = performance.now();
-      const execution = originalMethod.apply(this, args);
-      if (originalMethod.then) {
-        execution.then((result) =>
-          handleElaborate({ start, name, log: options.log, args, result })
-        );
-      } else {
-        handleElaborate({
-          start,
-          name,
-          log: options.log,
-          args,
-          result: execution
-        });
-      }
-      return execution;
+      return performanceHook(options)(originalMethod).apply(this, args);
     };
   };
 }
@@ -64,20 +47,35 @@ function hasPassedValidation({
   );
 }
 
-function handleElaborate({
-  start,
-  name,
-  log,
-  args,
-  result
-}: {
-  start: number;
-  name: string;
-  log: LogPerformanceOptionsLog;
-  args: any[];
-  result: any;
-}) {
-  const end = performance.now();
-  const executionTime = end - start;
-  log({ name, executionTime, args, result });
+export function logPerformance(_options: Partial<LogPerformanceOptions> = {}) {
+  const options: LogPerformanceOptions = {
+    ...LogPerformanceDefault,
+    ..._options
+  };
+  if (!options.name) {
+    throw new Error(
+      'Name is required for logPerformance higher-order function'
+    );
+  }
+
+  return performanceHook(options);
+}
+
+function performanceHook(options: LogPerformanceOptions) {
+  return hook({
+    before: ({ context }) => {
+      context.start = performance.now();
+    },
+    after: async ({ context, result, args }) => {
+      if (result.then) result = await result;
+
+      if (hasPassedValidation({ validation: options.validation, args }))
+        options.log({
+          name: options.name as string,
+          executionTime: performance.now() - context.start,
+          args,
+          result
+        });
+    }
+  });
 }
